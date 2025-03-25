35c835e41615a296d4e262ccdd228957e9be1e22
did99 <108026763+did99@users.noreply.github.com>
2025-03-25 17:03:12 -0300

    Add files via upload
    execicios

---
+++ Ex 2.js
@@ -0,0 +1,8 @@
+numero = 0
+if(numero > 0) {
+    console.log('O número é positivo!');
+} else if(numero === 0) {
+    console.log('O número é zero!');
+} else{
+    console.log('O número é negativo!');
+}
